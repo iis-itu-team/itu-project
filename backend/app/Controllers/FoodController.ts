6683dacaadd5a3e98712716fdd72bbbf8e64ce2f
Wertik1206 <jregvx@gmail.com>
2023-11-05 15:45:19 +0100

    fix(be): build error, unused import feat(be): make build work

--- backend/app/Controllers/FoodController.ts
+++ backend/app/Controllers/FoodController.ts
@@ -1,5 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { schema } from '@ioc:Adonis/Core/Validator'
 import FailureException from 'App/Exceptions/FailureException';
 
 import FoodService from "App/Services/FoodService";
