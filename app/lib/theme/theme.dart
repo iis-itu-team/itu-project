dc17d17d73d82ca1acf127e3e6bf85aa6ee31557
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-12-08 01:12:45 +0100

    feat(BE): default theme

---
+++ app/lib/theme/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const COLOR_PRIMARY = Color(0xFFFF8730);
+const COLOR_SECONDART = Color(0xFFF4EBDC);
+const COLOR_TEXT = Color(0xFF000000);
+
+class ThemeClass {
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: COLOR_PRIMARY,
+    floatingActionButtonTheme:
+        FloatingActionButtonThemeData(backgroundColor: COLOR_PRIMARY),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(16.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+    ),
+  );
+}
