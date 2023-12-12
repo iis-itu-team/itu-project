8ca3e623bf4c0d972cf8ea4f4cdf0a389388ba18
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-12-12 01:00:59 +0100

    feat(BE): buttomNavigationBar, text style change

--- app/lib/src/pages/order/order_page.dart
+++ app/lib/src/pages/order/order_page.dart
@@ -14,120 +14,142 @@ class OrderPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Home'), backgroundColor: COLOR_PRIMARY),
       body: Container(
-          color: COLOR_SECONDARY,
-          padding: const EdgeInsets.only(left: 16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text("Shrnutí", style: TextStyle(color: COLOR_TEXT)),
+        color: COLOR_SECONDARY,
+        padding: const EdgeInsets.only(left: 16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Text("Shrnutí",
+                    style: TextStyle(
+                        color: COLOR_TEXT,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text("Doručení",
+                          style: TextStyle(
+                              color: COLOR_TEXT,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Adjust this based on your layout requirements
+                        children: [
+                          Text(
+                            "Mesto",
+                            style: TextStyle(color: COLOR_TEXT),
+                          ),
+                          TextFormField(),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Adjust this based on your layout requirements
+                        children: [
+                          Text("Ulice"),
+                          TextFormField(),
+                          Text("č.p."),
+                          TextFormField(),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Adjust this based on your layout requirements
+                        children: [
+                          Text(
+                            "poznámka pro řidiče",
+                            style: TextStyle(color: COLOR_TEXT),
+                          ),
+                          TextFormField(),
+                        ],
+                      ),
+                    ),
+                    CheckboxListTile(
+                        value: false,
+                        onChanged: (val) {},
+                        activeColor: COLOR_SECONDARY,
+                        title: Text("pred dom")),
+                    CheckboxListTile(
+                        value: false,
+                        onChanged: (val) {},
+                        activeColor: COLOR_SECONDARY,
+                        title: Text("ku dverám")),
+                    CheckboxListTile(
+                        value: false,
+                        onChanged: (val) {},
+                        activeColor: COLOR_SECONDARY,
+                        title: Text("ku dverám bytu")),
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Adjust this based on your layout requirements
+                        children: [
+                          Text("patro"),
+                          TextFormField(),
+                          Text("číslo bytu"),
+                          TextFormField(),
+                        ],
+                      ),
+                    ),
+                    const Text("platba",
+                        style: TextStyle(
+                            color: COLOR_TEXT,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold)),
+                    CheckboxListTile(
+                        value: false,
+                        onChanged: (val) {},
+                        activeColor: COLOR_SECONDARY,
+                        title: Text("pri doručení (karta/hotovosť)")),
+                    CheckboxListTile(
+                        value: false,
+                        onChanged: (val) {},
+                        activeColor: COLOR_SECONDARY,
+                        title: Text("online kartou")),
+                    CheckboxListTile(
+                        value: false,
+                        onChanged: (val) {},
+                        activeColor: COLOR_SECONDARY,
+                        title: Text("paypal / google pay / apple pay"))
+                  ],
                 ),
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(16.0),
-                        child: Text("Doručení",
-                            style: TextStyle(color: COLOR_TEXT)),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment
-                              .start, // Adjust this based on your layout requirements
-                          children: [
-                            Text(
-                              "Mesto",
-                              style: TextStyle(color: COLOR_TEXT),
-                            ),
-                            TextFormField(),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment
-                              .start, // Adjust this based on your layout requirements
-                          children: [
-                            Text("Ulice"),
-                            TextFormField(),
-                            Text("č.p."),
-                            TextFormField(),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment
-                              .start, // Adjust this based on your layout requirements
-                          children: [
-                            Text(
-                              "poznámka pro řidiče",
-                              style: TextStyle(color: COLOR_TEXT),
-                            ),
-                            TextFormField(),
-                          ],
-                        ),
-                      ),
-                      CheckboxListTile(
-                          value: false,
-                          onChanged: (val) {},
-                          activeColor: COLOR_SECONDARY,
-                          title: Text("pred dom")),
-                      CheckboxListTile(
-                          value: false,
-                          onChanged: (val) {},
-                          activeColor: COLOR_SECONDARY,
-                          title: Text("ku dverám")),
-                      CheckboxListTile(
-                          value: false,
-                          onChanged: (val) {},
-                          activeColor: COLOR_SECONDARY,
-                          title: Text("ku dverám bytu")),
-                      Container(
-                        padding: EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment
-                              .start, // Adjust this based on your layout requirements
-                          children: [
-                            Text("patro"),
-                            TextFormField(),
-                            Text("číslo bytu"),
-                            TextFormField(),
-                          ],
-                        ),
-                      ),
-                      const Text("platba"),
-                      CheckboxListTile(
-                          value: false,
-                          onChanged: (val) {},
-                          activeColor: COLOR_SECONDARY,
-                          title: Text("pri doručení (karta/hotovosť)")),
-                      CheckboxListTile(
-                          value: false,
-                          onChanged: (val) {},
-                          activeColor: COLOR_SECONDARY,
-                          title: Text("online kartou")),
-                      CheckboxListTile(
-                          value: false,
-                          onChanged: (val) {},
-                          activeColor: COLOR_SECONDARY,
-                          title: Text("paypal / google pay / apple pay"))
-                    ],
-                  ),
-                ),
-              ],
-              // Divider(color: Colors.black)),
+              ),
+            ],
+            // Divider(color: Colors.black)),
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 100,
+        color: COLOR_PRIMARY,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton.large(
+              onPressed: () {},
+              backgroundColor: COLOR_SECONDARY,
+              child:
+                  const Text('Objednat!', style: TextStyle(color: COLOR_TEXT)),
             ),
-          )),
-      floatingActionButton: FloatingActionButton.large(
-        onPressed: () {},
-        child: const Text('Objednat!'),
-        backgroundColor: COLOR_PRIMARY,
+          ],
+        ),
       ),
     );
   }
