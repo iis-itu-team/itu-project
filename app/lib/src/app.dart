301d0b57d13ac3893925aed5ae3a2c173b6b16f3
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-12-12 20:56:40 +0100

    fix(FE): another dead code

--- app/lib/src/app.dart
+++ app/lib/src/app.dart
@@ -8,7 +8,6 @@ import 'package:food_blueprint/src/pages/burger_edit/burger_edit_page.dart';
 import 'package:food_blueprint/src/pages/home/home_controller.dart';
 import 'package:food_blueprint/src/pages/home/home_page.dart';
 import 'package:food_blueprint/src/pages/order_new/order_new_page.dart';
-import 'package:food_blueprint/src/pages/order_new/order_confirm_page.dart';
 import 'package:food_blueprint/src/pages/order_new/order_new_controller.dart';
 import 'package:food_blueprint/src/pages/order_show/order_show_page.dart';
 import 'package:food_blueprint/src/pages/order_show/order_show_controller.dart';
